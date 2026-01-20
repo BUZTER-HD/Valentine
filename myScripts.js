16320b65fc4c58a5c8217b3b272987b151a9b071
BUZTER-HD <74527525+BUZTER-HD@users.noreply.github.com>
2026-01-19 22:29:44 -0500

    Add files via upload

---
+++ myScripts.js
@@ -0,0 +1,9 @@
+
+
+
+function changeSize() {
+    document.getElementById("btnY").style.width = "500px";
+    document.getElementById("btnY").style.height = "500px";
+    document.getElementById("btnY").style.fontSize = "350px";
+    
+}
