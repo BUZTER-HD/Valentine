236c9321891bece0332fe5d881ec6937c53ab784
BUZTER-HD <74527525+BUZTER-HD@users.noreply.github.com>
2026-01-28 17:36:44 -0500

    Add files via upload

--- myScripts.js
+++ myScripts.js
@@ -1,9 +1,15 @@
+let yesSize = 1;
+let noTxtIndex= 0;
 
+const message = ["LOH BKT?!","DI MO NA PALA AKO LAB","MAY IBA KA NA SIGURO", "CRY NALANG AKO SA GILID",":<"];
 
+function changeSize()
+{
+    yesSize += 0.5;
+    const yesBtn = document.getElementById('btnY');
+    yesBtn.style.transform =`scale(${yesSize})`;
 
-function changeSize() {
-    document.getElementById("btnY").style.width = "500px";
-    document.getElementById("btnY").style.height = "500px";
-    document.getElementById("btnY").style.fontSize = "350px";
-    
+    noTxtIndex = (noTxtIndex + 1) % message.length;
+    document.getElementById('btnN').innerText = message[noTxtIndex];
+
 }
