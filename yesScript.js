1abcab519ce61841f9edaafaad97d4f480563aa7
BUZTER-HD <74527525+BUZTER-HD@users.noreply.github.com>
2026-02-12 22:04:09 -0500

    Add files via upload

---
+++ yesScript.js
@@ -0,0 +1,5 @@
+function displayMessage() {
+    document.getElementById('message').innerHTML = 
+    "<p style='font-size: 1.5em; color: #ff4081;'>Even from miles away, you're right here with me. Always. <br> I’m really grateful for you.<br>I’m glad it’s you.</p>";
+      
+}
